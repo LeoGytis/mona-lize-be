17d94a73d990561795691fa9606a341787599fb7
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-06-06 10:53:25 +0300

    save

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ const app = express();
 
 const username = encodeURIComponent(process.env.DB_USERNAME);
 const password = encodeURIComponent(process.env.DB_PASSWORD);
-// const dbURI = `mongodb+srv://${username}:${password}@nodeblog.wo7j5.mongodb.net/mona-lize?retryWrites=true&w=majority&appName=nodeblog`;
 const dbURI = `mongodb+srv://${username}:${password}@mona-lize.oxkq4p6.mongodb.net/?retryWrites=true&w=majority&appName=mona-lize`;
-// mongodb+srv://leogytis:kj0PRQqim3LgtEVQ@mona-lize.oxkq4p6.mongodb.net/
 
 mongoose
 
@@ -24,7 +22,13 @@ mongoose
 	.catch((err) => console.log(err));
 
 // Apply basic middleware first
-app.use(cors());
+app.use(
+	cors({
+		origin: ['http://localhost:3000', 'https://mona-lize.vercel.app'],
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
